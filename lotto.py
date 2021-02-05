95981673f0281e0c2841ae6510815b7af819bfec
Bredoto <serg.rumyantsev@gmail.com>
2021-02-05 20:41:34 +0200

    fresh

---
+++ lotto.py
@@ -0,0 +1,42 @@
+from random import random, sample
+from random import seed
+from random import randint
+from random import choice
+
+
+def input():
+    test = [1, 2, 3, 4, 5, 6]
+    return test
+
+
+def generate():
+    sequence = [i for i in range(1, 53)]
+    # print(sequence)
+    # make choices from the sequence
+    for _ in range(1):
+        # selection = choice(sequence)
+        selection = sorted(sample(sequence, 6))
+        # sorted(sequence)
+        # print((selection))
+        return selection
+
+
+def compare(user_input, generated_output):
+    for i in range(len(user_input)):
+        cc:list [i] = user_input[i] - generated_output[i]
+    print(cc)
+    return cc
+
+
+def output():
+    pass
+
+
+def main():
+    print(generate())
+    print(input())
+    print(compare(input(), generate()))
+
+
+if __name__ == "__main__":
+    main()
